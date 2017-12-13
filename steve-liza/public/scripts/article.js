bdbb4f17ce3e504713d7733a97f65cc4ff8bade4
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-13 11:40:08 -0800

    Added a comment about the optional callback

--- steve-liza/public/scripts/article.js
+++ steve-liza/public/scripts/article.js
@@ -50,7 +50,7 @@ Article.prototype.insertRecord = function(callback) {
       console.log(data);
 
       // COMMENT: What is the purpose of this line? Is the callback invoked when this method is called? Why or why not?
-      // PUT YOUR RESPONSE HERE
+      // This is an optional callback that will be executed after creating a new article. e.g. route user back to index.html
       if (callback) callback();
     })
 };
