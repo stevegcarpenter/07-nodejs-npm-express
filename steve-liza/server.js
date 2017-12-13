b44c654cf00e03ccbdf12778fbe04614c75d96c8
Victoria Oh <veoh1989@gmail.com>
2017-12-13 10:39:16 -0800

    added route for /new that serves up new.html

--- steve-liza/server.js
+++ steve-liza/server.js
@@ -4,7 +4,6 @@
 
 // imports
 const express = require('express');
-
 const app = express();
 // We point express to the public folder where all static files are located
 // the public folder should only contain static project files
@@ -12,6 +11,11 @@ app.use(express.static('./public'));
 
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+
+//if the user types /new route they will be served up new.html 
+app.get('/new', (req, res) => {
+  res.status(200).sendfile('./public/new.html');
+});
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
